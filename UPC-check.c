8d948806dbf703a47ed7aed263847c3d3701cb8e
benjamin-turnbull <benjamin_turnbull@outlook.com>
2022-01-15 15:49:14 +1030

    fixed mistake

--- UPC-check.c
+++ UPC-check.c
@@ -13,7 +13,7 @@ int main(void)
     second_sum = i1 + i3 + i5 + j2 + j4;
     total = 3 * first_sum + second_sum;
 
-    printf("Check digit: %d\n", 9 - ((total - 1) % 1));
+    printf("Check digit: %d\n", 9 - ((total - 1) % 10));
 
     return 0;
 }
